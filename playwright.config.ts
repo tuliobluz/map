fce79815a32bdd483365a37d326e2fd3bffb6f62
Tulio Bandeira Luz <tulio8395@gmail.com>
2024-09-06 07:59:11 -0400

    ST-01: Add search scenarios, add testidAtt on config

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,10 +25,12 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.BASE_URL || 'https://maps.google.com/maps',
+    baseURL: process.env.BASE_URL || 'https://www.google.com/maps?hl=en',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+
+    testIdAttribute: 'id'
   },
 
   /* Configure projects for major browsers */
--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -1,8 +1,38 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('/');
+test.describe('User search location', async () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('');
+  });
+  test('basic', async ({ page }) => {
+    const newTodo = await page.getByTestId('searchboxinput');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Google Maps/);
+    await newTodo.fill('Paris');
+    await page.getByTestId('searchbox-searchbutton').click();
+
+    await expect(page
+      .getByRole('main')
+      .filter({ hasText: 'Paris' })).toBeVisible();
+  });
+
+  test('with directions', async ({ page }) => {
+    const newTodo = await page.getByTestId('searchboxinput');
+
+    await newTodo.fill('London');
+    await page.getByTestId('searchbox-searchbutton').click();
+
+    const headline = page
+      .getByRole('main')
+      .filter({ hasText: 'London' });
+
+    await expect(headline).toBeVisible;
+
+    await page.getByText('Directions').click();
+
+    const destination = page
+      .getByRole('listitem')
+      .filter({ hasText: 'London' });
+
+    await expect(destination).toBeVisible();
+  });
 });
